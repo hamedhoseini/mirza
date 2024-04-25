b4b0d14bbeb683791100ac47ecfbac8c7546cd56
Ehsan <eaehsanea@gmail.com>
2024-04-25 12:32:17 +0330

    main menu text size and padding corrected productSubmit and customerSubmit navigation set

--- app/src/main/java/com/example/almas/MainActivity.kt
+++ app/src/main/java/com/example/almas/MainActivity.kt
@@ -4,13 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import android.widget.Button
 import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var titrBtn : CardView
     private lateinit var saleBtn : CardView
+    private lateinit var productSubmitBtn : CardView
+    private lateinit var customerSubmitBtn : CardView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +36,17 @@ class MainActivity : AppCompatActivity() {
            var intent = Intent(this , Title :: class.java )
             startActivity(intent)
 
+        }
+        productSubmitBtn = findViewById(R.id.product_submit_btn)
+        productSubmitBtn.setOnClickListener {
+            var intent3 = Intent(this , ProductSubmit :: class.java )
+            startActivity(intent3)
+        }
+
+        customerSubmitBtn = findViewById(R.id.customer_submit_btn)
+        customerSubmitBtn.setOnClickListener {
+            var intent4 = Intent(this , CustumerSubmit :: class.java )
+            startActivity(intent4)
         }
 
 
